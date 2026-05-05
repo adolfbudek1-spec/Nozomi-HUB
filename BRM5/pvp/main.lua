c5484f515ab1d1c708d9e4041a419141e46364d5
theofitzgerald <adolfbudek1@gmail.com>
2026-05-05 15:21:35 +0700

    s

---
+++ BRM5/pvp/main.lua
@@ -0,0 +1,53 @@
+local Velvetrepo = "https://raw.githubusercontent.com/DexCodeSX/Velvet/main/"
+local Velvet = loadstring(game:HttpGet(Velvetrepo .. "Library.lua"))()
+local Icons = loadstring(game:HttpGet(Velvetrepo .. "addons/Icons.lua"))()
+local QuickBar = loadstring(game:HttpGet(Velvetrepo .. "addons/QuickBar.lua"))()
+local NotifHistory = loadstring(game:HttpGet(Velvetrepo .. "addons/NotificationHistory.lua"))()
+local Nozomirepo = "https://raw.githubusercontent.com/theofitzgerald/BRM5/main/"
+local config = loadstring(game:HttpGet(Nozomirepo.. "BRM5/pvp/module.config.lua"))
+
+
+--======================== [[ CREATE WINDOW ]] ========================
+local Window = Velvet:CreateWindow({
+    Title = "Velvet",
+    SubTitle = "v3.2 showcase",
+    ToggleKey = Enum.KeyCode.RightShift,
+    ToggleIcon = "sparkles",
+})
+
+--======================== [[ BIND ADDON ]] ========================
+Velvet:SetIcons(Icons)
+NotifHistory:Bind(Velvet, Window)
+
+--========= [[ VISUAL TAB ]] =========
+local visualTab = Window:AddTab("Visual", "eye")
+local ESPSection_1 = visualTab:AddSection("Esp Player")
+ESPSection_1:AddToggle("ESPEnable_1", {
+    Text = "Enable ESP",
+    Default = config.espPlayerEnabled,
+    Callback = function(v) 
+        print("ESP ACTIVATED")
+    end
+})
+
+ESPSection_1:AddToggle("ESPHighlight_1", {
+    Text = "Show Highlight",
+    Default = config.espPlayerHighlight,
+    VisibleWhen = "ESPEnable_1",
+    Callback = function(v) 
+        print("ESP ACTIVATED")
+    end
+})
+
+ESPSection_1:AddColorPicker("ESPColor_1", {
+    Text = "ESP Color",
+    Default = config.espPlayerColor,
+    VisibleWhen = "ESPEnable_1",
+})
+
+ESPSection_1:AddSlider("ESPDistance", {
+    Text = "Max Distance",
+    Min = 100, Max = 5000, Default = config.espPlayerDistance, Increment = 50,
+    Suffix = " studs",
+    VisibleWhen = "ESPEnable_1",
+})
---
+++ BRM5/pvp/module/config.lua
@@ -0,0 +1,8 @@
+local config = {}
+
+config.espPlayerEnabled = false
+config.espPlayerHighlight = false
+config.espPlayerColor = Color3.fromRGB(255, 255 255)
+config.espPlayerDistance = 2000
+
+return config
