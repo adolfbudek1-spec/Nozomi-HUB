e7f34df92ebc89e0eb044fde2a54f066df782e02
theofitzgerald <adolfbudek1@gmail.com>
2026-05-06 00:43:25 +0700

    s

---
+++ BRM5/pvp/module/highlight.lua
@@ -0,0 +1,15 @@
+local highlight = {
+	tracked = {},
+	connection = {}
+}
+
+--[[ DESTROY ALL HIGHLIGHT ]]
+--[[ UNTRACKED HIGHLIGHT ]]
+--[[ CREATE HIGHLIGHT ]]
+--[[ REGISTER MODEL ]]
+--[[ REFRESH TARGET MODEL ]]
+--[[ SETUP LISTENER ]]
+--[[ SET HIGHLIGHT ENABLE ]]
+--[[ CLEANUP ]]
+
+return highlight
