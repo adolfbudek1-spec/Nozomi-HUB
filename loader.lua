0fafb826b1cde80570b041469c4fc92813c13335
theofitzgerald <adolfbudek1@gmail.com>
2026-05-15 05:07:20 +0700

    s

---
+++ loader.lua
@@ -0,0 +1,6 @@
+local gameIds = {
+    ["Blackhawk rescue mission 5"] = {
+        url = "https://github.com/theofitzgerald/Nozomi-HUB/main/BRM5/main.lua",
+        placeId = {5289429734, 5480112241, 4524359706, 5468388011, 3701546109}
+    }
+}
